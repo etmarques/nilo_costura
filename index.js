d5238ab68b76e280f2083bd24d19f8d6b265a5de
Etienne Marques <97489119+etmarques@users.noreply.github.com>
2022-04-19 17:02:20 -0300

    delete_v01

--- index.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.header__menu')
-const menu = document.querySelector('.menu')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu--ativo')
-})
